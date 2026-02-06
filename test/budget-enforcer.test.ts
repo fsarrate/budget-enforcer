6821bd405b47aa2b86a1892a4779ae87270a3917
Francesc Sarrate <satate@gmail.com>
2026-02-06 09:57:41 +0100

    Initial commit

---
+++ test/budget-enforcer.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BudgetEnforcer from '../lib/budget-enforcer-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/budget-enforcer-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BudgetEnforcer.BudgetEnforcerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
